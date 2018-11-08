2614aa77c1c5fee9d54c500922ea23b3139d9605
Takashi OYA <oya@pnrm.co.jp>
2018-11-08 13:31:07 +0900

    記事一覧、詳細を作成

---
+++ pages/articles/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      <nuxt-link to="/articles/" class="button--grey">ブログ一覧へ戻る</nuxt-link>
+      <h1 class="title">タイトル</h1>
+      <div class="text">
+        本文
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  text-align: left;
+}
+
+.title {
+  font-size: 30px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <nuxt-link to="/articles/" class="button--grey">ブログ一覧へ</nuxt-link>
       </div>
     </div>
   </section>
